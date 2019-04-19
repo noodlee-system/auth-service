0ed3c05d3801c05837fdfa19bd2d20576b8cb2ff
SkrzypMajster <skrzypmajster@gmail.com>
2019-04-19 16:24:21 +0200

    Remove comment

--- src/main/java/com/noodleesystem/auth/AuthController.java
+++ src/main/java/com/noodleesystem/auth/AuthController.java
@@ -4,7 +4,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
   
 @RestController
-public class AuthController { // nazwa klasy 
+public class AuthController {
     @RequestMapping("/auth")
     public String auth() { 
         return "Hello World! :)"; 
