7cc52ddbd3a12ccb1b868e8538456410601525b4
SkrzypMajster <skrzypmajster@gmail.com>
2019-04-27 14:32:47 +0200

    Create AuthController that returns JSON with mock token

--- src/main/java/com/noodleesystem/auth/AuthController.java
+++ src/main/java/com/noodleesystem/auth/AuthController.java
@@ -1,12 +1,24 @@
 package com.noodleesystem.auth;
   
-import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.HashMap;
+import java.util.Map;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
   
 @RestController
 public class AuthController {
-    @RequestMapping("/auth")
-    public String auth() { 
-        return "Hello World! :)"; 
+
+    @CrossOrigin(origins = "http://localhost")
+    @PostMapping("/auth")
+    public Map<String, String> auth(@RequestBody AuthRequestBody request) { 
+        String username = request.getUsername();
+        HashMap<String, String> responseMap = new HashMap<>();
+        
+        // generate mock token 
+        responseMap.put("token", "b3a5fed2-93c3-40d8-aa28-a0a0bd0391e3");
+   
+        return responseMap; 
     }
 }
---
+++ src/main/java/com/noodleesystem/auth/AuthRequestBody.java
@@ -0,0 +1,19 @@
+package com.noodleesystem.auth;
+
+public class AuthRequestBody {
+	private String username;
+	private String password;
+
+	AuthRequestBody(String username, String password) {
+		this.username = username;
+		this.password = password;
+	}
+
+    public String getUsername() {
+        return this.username;
+    }
+
+    public String getPassword() {
+        return this.password;
+    }
+}
