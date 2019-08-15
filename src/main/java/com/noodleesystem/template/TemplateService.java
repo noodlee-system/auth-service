82f9cc315ff8d266e76392d2e2b8a213eb1c2e8c
SkrzypMajster <skrzypmajster@gmail.com>
2019-08-15 13:54:24 +0200

    Create Serilog and RabbitMQ configuration classes

--- src/main/java/com/noodleesystem/template/TemplateService.java
+++ src/main/java/com/noodleesystem/template/TemplateService.java
@@ -1,29 +1,15 @@
 package com.noodleesystem.template;
 
-import static serilogj.sinks.coloredconsole.ColoredConsoleSinkConfigurator.*;
-import static serilogj.sinks.seq.SeqSinkConfigurator.*;
-
-import serilogj.Log;
-import serilogj.LoggerConfiguration;
-import serilogj.core.LoggingLevelSwitch;
-import serilogj.events.LogEventLevel;
-import serilogj.sinks.seq.SeqSink;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.noodleesystem.template.model.User;
+import serilogj.Log;
 
 @SpringBootApplication
 public class TemplateService {
-	public static void main(String[] args) {
-		Log.setLogger(new LoggerConfiguration()
-			.writeTo(coloredConsole())
-			.writeTo(seq("http://noodlee_seq:5341"))
-			.createLogger());
-
+    public static void main(String[] args) {
 		SpringApplication.run(TemplateService.class, args);
 
-		Log.information("{serviceName} is running...", "Template Service");
+        Log.information("{serviceName} is running...", "Template Service");
 	}
 }
--- src/main/java/com/noodleesystem/template/config/SwaggerConfig.java
+++ src/main/java/com/noodleesystem/template/config/SwaggerConfig.java
@@ -1,7 +1,5 @@
 package com.noodleesystem.template.config;
 
-import static springfox.documentation.builders.PathSelectors.regex;
-import java.util.Collections;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
@@ -18,20 +16,22 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
     @Bean
     public Docket api() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-            .apis(RequestHandlerSelectors
-                .basePackage("com.noodleesystem.template"))
-            .paths(PathSelectors.regex("/.*"))
-            .build()
-            .apiInfo(apiEndPointsInfo());
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.noodleesystem.template"))
+                .paths(PathSelectors.regex("/.*"))
+                .build()
+                .apiInfo(apiEndPointsInfo());
     }
+
     private ApiInfo apiEndPointsInfo() {
-        return new ApiInfoBuilder().title("Template Service REST API")
-            .description("Noodlee System Template Service REST API")
-            .contact(new Contact("NoodleeSystem", "www.noodleesystem.com", "contact@noodleesystem.com"))
-            .license("Apache 2.0")
-            .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
-            .version("1.0.0")
-            .build();
+        return new ApiInfoBuilder()
+                .title("Template Service REST API")
+                .description("Noodlee System Template Service REST API")
+                .contact(new Contact("NoodleeSystem", "www.noodleesystem.com", "contact@noodleesystem.com"))
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .version("1.0.0")
+                .build();
     }
 }
