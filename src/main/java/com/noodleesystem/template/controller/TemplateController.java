4b22ac35e24676639f8c533963f6a01d80bd9833
SkrzypMajster <skrzypmajster@gmail.com>
2019-08-15 23:24:44 +0200

    Configure example communication with RabbitMQ queue

--- src/main/java/com/noodleesystem/template/controller/TemplateController.java
+++ src/main/java/com/noodleesystem/template/controller/TemplateController.java
@@ -1,51 +1,55 @@
-package com.noodleesystem.template;
-  
-import java.util.HashMap;
-import java.util.Map;
+package com.noodleesystem.template.controller;
+
+import java.text.MessageFormat;
 import java.util.List;
 
+import com.noodleesystem.template.exception.EmptyQueueException;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
-
 import com.noodleesystem.template.model.User;
 import com.noodleesystem.template.repository.UserRepository;
+import org.springframework.web.client.HttpClientErrorException;
+import serilogj.Log;
 
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
-  
 @RestController
 @RequestMapping("/api")
 public class TemplateController {
+    final static String queueName = "template_queue";
+
     @Autowired
 	private UserRepository usersRepository;
 
     @Autowired
-    private RabbitTemplate rabbitTemplate;
+    RabbitTemplate rabbitTemplate;
 
     @GetMapping("/users")
-	public List<User> getAllUsers() {
-		return usersRepository.findAll();
+    List<User> getAllUsers() {
+        return usersRepository.findAll();
 	}
 
-    @GetMapping("/addMessage")
-    public String get(@RequestParam String message) {
-        rabbitTemplate.convertAndSend("test", message);
-        return "Message sent into 'test' queue!";
+    @GetMapping("/send")
+    String sendToQueue(@RequestParam(value = "message", defaultValue = "Default message") String message) {
+        rabbitTemplate.convertAndSend(queueName, message);
+        Log.information("{message} message was sent to {queue} queue!",message, queueName);
+        return String.format("Message %s sent!", message);
     }
 
-    @GetMapping("/receiveMessage")
+    @GetMapping("/receive")
     public String get() {
-        Object message = rabbitTemplate.receiveAndConvert("test");
-        
-        if (message != null) {
-            return message.toString();
+        Object messageObject = rabbitTemplate.receiveAndConvert(queueName);
+
+        if (messageObject != null) {
+            String message = messageObject.toString();
+            Log.information("{message} message was read from {queue} queue!", message, queueName);
+            return message;
         } else {
-            return "No message in queue 'test'!";
+            String errorMessage = MessageFormat.format("No message in queue {0}!", queueName);
+            throw new EmptyQueueException(errorMessage);
         }
     }
 }
